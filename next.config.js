a161965f57fc290be4a1f9ab5021bcb0dcfe9beb
Lucas Almeida <lucsa.dev@gmail.com>
2022-08-31 21:05:29 -0300

    feat: add next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    /* config options here */
+    basePath: "nextjs-pages",
+    assetPrefix: "nextjs-pages"
 }
 module.exports = nextConfig
