0b0b9bd47370c5527a0496a0dfeb0a8fe68d3bc2
Lucas Almeida <lucsa.dev@gmail.com>
2022-09-15 16:10:11 -0300

    feat: add repo env in components

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -10,7 +10,7 @@ type Props = {
 const CoverImage = ({ title, src, slug }: Props) => {
   const image = (
     <img
-      src={"/blog" + src}
+      src={process.env.NODE_ENV + src}
       alt={`Cover Image for ${title}`}
       className={cn('shadow-sm', {
         'hover:shadow-lg transition-shadow duration-200': slug,
