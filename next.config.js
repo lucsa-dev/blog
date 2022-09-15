837f9ea4ba110b2f6d551772968d0fff83689dfa
Lucas Almeida <lucsa.dev@gmail.com>
2022-09-15 16:09:02 -0300

    feat: add repo name in .env file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    basePath: "/blog",
-    assetPrefix: "/blog"
+    basePath: process.env.REPO_NAME,
+    assetPrefix: process.env.REPO_NAME
 }
 module.exports = nextConfig
