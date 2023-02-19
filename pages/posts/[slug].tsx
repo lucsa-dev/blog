b633cc5371df755d1eea59bc66d319661f5dc802
Lucas Almeida <lucsa.dev@gmail.com>
2023-02-18 22:13:27 -0300

    feat: change title

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -35,7 +35,7 @@ export default function Post({ post, morePosts, preview }: Props) {
             <article className="mb-32">
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {post.title} | lucsa.dev
                 </title>
                 {post.ogImage && <meta property="og:image" content={post.ogImage.url} />}
               </Head>
